fc68ca3470da91d4695c8b8d628dc5373cf99591
kunj-21 <107642217+kunj-21@users.noreply.github.com>
2024-04-08 21:23:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function dayOfYear(year) {
+	if(years % 400 !== 0 )
+	{
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
+dayOfYear(2022)
+dayOfAYear(2024)	
+dayOfAYear(1900)	
+dayOfAYear(2000)
